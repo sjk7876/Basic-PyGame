496eafd30cfb1fdf7783b55379443bbefd970d05
spenc <spence4826@gmail.com>
2021-10-05 11:57:57 -0400

    you got hit!

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
+import random
 import pygame
+from pygame.locals import *
+
+if not pygame.font: print('Warning, fonts disabled')
 
 colors = {'black': (0, 0, 0),
           'white': (255, 255, 255),
           'red': (255, 0, 0),
           'green': (0, 255, 0),
           'blue': (0, 0, 255)}
+color_names = ('black', 'white', 'red', 'green', 'blue')
+
 screen_width = 720
 screen_height = 480
 screen_size = (screen_width, screen_height)
@@ -74,24 +80,55 @@ def hitEdge(smileBox2, smileBox):
 def main():
 	screen = initialize()
 	active = True
-	clockobject = pygame.time.Clock()
-	move = 10
+	clockObject = pygame.time.Clock()
+	move = 5
 	
 	smile = pygame.image.load('smile_40x40.png').convert_alpha()
+	# smileBad = pygame.image.load('smile_20x20.png').convert_alpha()
 	smileBox = smile.get_rect()
 	smileBox2 = smile.get_rect()
 	
+	enemiesXY = []
+	
+	for i in range(40):
+		x = random.randint(0, screen_width)
+		y = random.randint(0, screen_height)
+		color = color_names[random.randint(1, len(color_names) - 1)]
+		enemiesXY.append((x, y, color))
+	
+	collisions = 0
+	frame = 0
+	totalHits = 0
+	
 	while active:
-		clockobject.tick(60)
+		clockObject.tick(60)
 		screen.fill(colors['black'])
-		corner = False
 		
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
+				print('# of Collisions:', totalHits)
 				active = False
 		
 		smileBox = movement(smileBox, move)
 		smileBox2, corner = hitEdge(smileBox2, smileBox)
+		
+		for i in enemiesXY:
+			pygame.draw.circle(screen, i[2], (i[0], i[1]), 3)
+			if smileBox.collidepoint(i[0], i[1]) or smileBox2.collidepoint(i[0], i[1]):
+				collisions += 1
+		
+		if frame < 30:
+			if collisions > 1:
+				frame += 1
+				if pygame.font:
+					font = pygame.font.Font(None, 36)
+					text = font.render("You Got Hit!", True, colors['white'], (0, 0, 0))
+					textpos = text.get_rect(centerx=screen.get_width() / 2)
+					screen.blit(text, textpos)
+		else:
+			collisions = 0
+			frame = 0
+			totalHits += 1
 		
 		if corner:
 			screen.blit(smile, smileBox2)
